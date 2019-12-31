f0e1052d5e41bd625547027e499e36b4edf9ce5a
germancuello <johnsmith93030@gmail.com>
2020-01-01 06:09:20 +0800

    Add custom style to chart component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,59 @@ import React, { Component } from 'react';
 import Chart from './components/Chart';
 import './App.css';
 
+const priceGivenOnSlider = 1500;
+const countOfYears = 10;
+const startYear=2021;
+const data = [
+  {
+    name: 'Banana',
+    currentPrice: 2,
+    CO2perTon: 0.000072,
+    carbonPriceToday: 200,
+  },
+  {
+    name: 'Burger',
+    currentPrice: 30,
+    CO2perTon: 0.004,
+    carbonPriceToday: 200,
+  },
+  {
+    name: 'Book',
+    currentPrice: 100,
+    CO2perTon: 0.0027,
+    carbonPriceToday: 200,
+  },
+  {
+    name: '1L oil',
+    currentPrice: 12,
+    CO2perTon: 0.002392,
+    carbonPriceToday: 200,
+  },
+  {
+    name: 'A flight to London',
+    currentPrice: 500,
+    CO2perTon: 0.212,
+    carbonPriceToday: 200,
+  }
+];
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>CHART EXAMPLE</h1>
-        <Chart />
-      </div>
+      <>
+        <h1>Chart Component</h1>
+        <div className="app-chart">
+          <Chart
+            height="100%"
+            width="100%"
+            backgroundColor="transparent"
+            data={data}
+            startYear={startYear}
+            countOfYears={countOfYears}
+            priceGivenOnSlider={priceGivenOnSlider}
+          />
+        </div>
+      </>
     );
   }
 }
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,17 +1,111 @@
 import React, { Component } from 'react';
+import range from 'lodash/range';
 import ReactEcharts from 'echarts-for-react';
 
-
-
 class Chart extends Component {
+  updateDataByAddtionalPrice() {
+    const { data, priceGivenOnSlider, countOfYears } = this.props;
+    const updatedData = data.map(item => {
+      const additionalPrice = (priceGivenOnSlider - item.carbonPriceToday) / countOfYears;
+      const { CO2perTon, currentPrice } = item;
+      item['prices'] = range(0, countOfYears).map(idx => additionalPrice * (idx + 1)  * CO2perTon + currentPrice);
+      return item;
+    });
+    return updatedData;
+  }
+
+  getEchartDefaultOption() {
+    return {
+      animation: true,
+      tooltip: {
+        trigger: 'item',
+        position: 'top',
+        axisPointer: {
+          type: 'shadow',
+        },
+        opacity: 0.9,
+        backgroundColor: '#ddd',
+        textStyle: {
+          color: '#000',
+        },
+        confine: true,
+      },
+      grid: {
+        left: 200,
+        right: 60,
+        bottom: 50,
+        top: 10,
+        containLabel: false,
+        show: true,
+        borderWidth: 0,
+      },
+      xAxis: [],
+      yAxis: [],
+      series: [],
+    };
+  }
+
   getChartOption = () => {
-    return {};
+    const { startYear, backgroundColor } = this.props;
+    const defaultOptions = this.getEchartDefaultOption();
+    const updatedData = this.updateDataByAddtionalPrice();
+    const options = {
+      grid: {
+        ...defaultOptions.grid,
+        borderWidth: 2,
+        backgroundColor,
+      },
+      legend: {
+        show: true,
+        orient: 'vertical',
+        left: 0,
+        data: updatedData.map(({ name }) => ({
+          name,
+          icon: 'circle',
+        })),
+      },
+      tooltip: {
+        ...defaultOptions.tooltip,
+        trigger: 'item',
+        formatter: ({ name, value }) => {
+          const firstLine = 'a money would cost<br/>';
+          const secondLine = `<b>${value.toFixed(2)} kr</b> in ${name}`;
+          return firstLine + secondLine;
+        },
+      },
+      xAxis: [{
+        type: 'category',
+        axisLine: { show: false },
+        axisTick: { show: false },
+        splitLine: { show: false },
+        data: range(startYear, startYear + this.props.countOfYears),
+      }],
+      yAxis: [{
+        name: 'Price',
+        nameLocation: 'middle',
+        nameGap: 35,
+        type: 'value',
+        axisLabel: {
+          formatter: value => (
+            value < 1000 ? value : `${value / 1000}k`
+          ),
+        }
+      }],
+      series: updatedData.map(({ name, prices }) => ({
+        name,
+        type: 'line',
+        data: prices,
+      })),
+    };
+
+    return options;
   }
 
   render() {
+    const { height, width } = this.props;
     return (
       <ReactEcharts
-        style={{ height: '100%', width: '100%' }}
+        style={{ height, width }}
         option={this.getChartOption()}
         notMerge
         lazyUpdate
